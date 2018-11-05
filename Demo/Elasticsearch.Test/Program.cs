3196c63ee5b713b5d0f4132524a1e9ac03e625a8
刘宝刚 <liubg@jinher.com>
2018-11-05 09:04:38 +0800

    2018-11-05

--- Demo/Elasticsearch.Test/Program.cs
+++ Demo/Elasticsearch.Test/Program.cs
@@ -1,39 +1,89 @@
-﻿using PlainElastic.Net;
-using PlainElastic.Net.Serialization;
+﻿using Elasticsearch.Net;
+using Nest;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace Elasticsearch.Test
 {
+    class Tweet
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public DateTime PostDate { get; set; }
+        public string Message { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var connection = new ElasticConnection("192.168.1.167", 9200);
+            //http://192.168.1.167:9200/_search
+            //http://192.168.1.167:9200/twitter/tweet/10
+
+            var node = new Uri("http://192.168.1.167:9200");
+            var settings = new ConnectionSettings(node);
+            settings.DefaultIndex("tweets");
+            var client = new ElasticClient(settings);
+
+            var list = new List<Tweet>();
+            for (int i = 1; i < 20; i++)
+            {
+                list.Add(new Tweet
+                {
+                    Id = i,
+                    UserName = "liubaogang-" + i,
+                    PostDate = DateTime.Now,
+                    Message = "Trying out NEST, so far so good?"
+                });
+            }
+            //var response = client.IndexMany<Tweet>(list);
+
+             
+            client.DeleteMany(list, "tweets", "tweet");
+
+            //或
+            //IDeleteRequest request = new DeleteRequest("tweets", "tweet", 16);
+            //client.Delete(request);
+            //request = new DeleteRequest("tweets", "tweet", 17);
+            //client.Delete(request);
+            //request = new DeleteRequest("tweets", "tweet", 18);
+            //client.Delete(request);
+            //request = new DeleteRequest("tweets", "tweet", 15);
+            //client.Delete(request);
+            //request = new DeleteRequest("tweets", "tweet", 2);
+            //client.Delete(request);
+            //request = new DeleteRequest("tweets", "tweet", 13);
+            //client.Delete(request);
 
 
-            var tweet = new Tweet { User = "刘宝刚" , Address="黑龙江省大庆市", Age="33" };
 
-            string tweetJson = JsonConvert.SerializeObject(tweet);
 
-            string result = connection.Put(new IndexCommand("twitter", "tweet", id: Guid.NewGuid().ToString()), tweetJson);
-            
+            //var result = client.Get<Tweet>(1, idx => idx.Index("tweets")); 
+            //var tweet_result = result.Source;
+
+            //var result1 = client.Search<Tweet>(s => s
+            //                .From(0)
+            //                .Size(10)
+            //                .Query(q => q
+            //                    .Term(t => t.UserName, "刘") || q
+            //                    .Match(mq => mq.Field(f => f.UserName).Query("nest"))
+            //                )
+            //            );
+
+            var result1 = client.Search<Tweet>(s => s
+                            //.AllTypes()
+                            //.AllIndices()
+                            //.Scroll( new Time(15))
+                            .From(0)
+                            .Size(10)
+                            .Query(q =>
+                                q.Match(mq => mq.Field(f => f.UserName).Query("刚"))
+                                 
+                            )
+                        );
+            //client.IndexMany(new List<Tweet>());
+
+           
         }
     }
-
-    internal class Tweet
-    {
-        public string User { get; set; }
-
-        public string Address { get; set; }
-
-        public string Age { get; set; }
-
-
-        public int big { get; set; }
-    }
+ 
 }
--- Demo/Test/Program.cs
+++ Demo/Test/Program.cs
@@ -10,6 +10,13 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            //模拟数据变化
+            //Task.Factory.StartNew(() => 
+            //{
+            //    System.Threading.Thread.Sleep(2000);
+            //    WeightedRoundRobin.Change();
+            //});
+            //正是测试开始
             Dictionary<string, int> dic = new Dictionary<string, int>();
             Server s;
             for (int j = 0; j < 100; j++)
@@ -20,11 +27,12 @@ namespace Test
                 if (!dic.Keys.Contains("服务器" + s.IP + ",权重:" + s.Weight))
                     dic.Add("服务器" + s.IP + ",权重:" + s.Weight, 0);
                 dic["服务器" + s.IP + ",权重:" + s.Weight]++;
+                System.Threading.Thread.Sleep(100);
             }
 
             foreach (var i1 in dic)
             {
-                Console.WriteLine("{0}共处理请求{1}次", i1.Key, i1.Value);
+                Console.WriteLine("{0}，共处理请求{1}次", i1.Key, i1.Value);
             }
 
             Console.ReadLine();
@@ -37,27 +45,45 @@ namespace Test
     public static class WeightedRoundRobin
     {
 
+        public static void Change()
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(2000);
+                s.Add(new Server()
+                {
+                    IP = "192.168.0.105",
+                    Weight = 30
+                });
+                System.Threading.Thread.Sleep(4000);
+                s.RemoveAt(s.Count - 1);
+
+                System.Threading.Thread.Sleep(40000000);
+
+            }
+        }
+
         private static List<Server> s = new List<Server>()
                 {
                     new Server()
                         {
                             IP = "192.168.0.100",
-                            Weight = 3
+                            Weight = 10
                         },
                     new Server()
                         {
                             IP = "192.168.0.101",
-                            Weight = 6
+                            Weight = 2
                         },
                     new Server()
                         {
                             IP = "192.168.0.102",
-                            Weight = 6
+                            Weight = 2
                         },
                     new Server()
                         {
                             IP = "192.168.0.103",
-                            Weight = 4
+                            Weight = 1
                         },
                     new Server()
                         {
@@ -67,10 +93,10 @@ namespace Test
                 }.OrderBy(a => a.Weight).ToList();
 
         private static int i = -1;//代表上一次选择的服务器
-        private static int gcd = GetGcd(s);//表示集合S中所有服务器权值的最大公约数
+        //private static int gcd = GetGcd(s);//表示集合S中所有服务器权值的最大公约数
         private static int cw = 0;//当前调度的权值
-        private static int max = GetMaxWeight(s);
-        private static int n = s.Count;//服务器个数
+        //private static int max = GetMaxWeight(s);
+        //private static int n = s.Count;//服务器个数
 
 
         /**
@@ -83,15 +109,15 @@ namespace Test
         {
             while (true)
             {
-                i = (i + 1) % n;
+                i = (i + 1) % s.Count;
                 if (i == 0)
                 {
-                    cw = cw - gcd;
+                    cw = cw - GetGcd(s);
                     if (cw <= 0)
                     {
-                        cw = max;
+                        cw = GetMaxWeight(s);
                         if (cw == 0)
-                            return null;
+                            return s[s.Count - 1];
                     }
                 }
                 if (s[i].Weight >= cw)
